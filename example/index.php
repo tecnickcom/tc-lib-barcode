d8bc9299530a4705bd4b278bf736a9008e0f6320
Nicola Asuni <info@tecnik.com>
2015-06-18 12:25:03 +0100

    updated dependencies and improved examples

--- example/index.php
+++ example/index.php
@@ -19,7 +19,62 @@ require ('../vendor/autoload.php');
 // autoloader when using RPM or DEB package installation
 //require ('/usr/share/php/Com/Tecnick/Barcode/autoload.php');
 
+// data to generate for each barcode type
+$linear = array(
+    'C128A'      => array('0123456789', 'CODE 128 A'),
+    'C128B'      => array('0123456789', 'CODE 128 B'),
+    'C128C'      => array('0123456789', 'CODE 128 C'),
+    'C128'       => array('0123456789', 'CODE 128'),
+    'C39E+'      => array('0123456789', 'CODE 39 EXTENDED + CHECKSUM'),
+    'C39E'       => array('0123456789', 'CODE 39 EXTENDED'),
+    'C39+'       => array('0123456789', 'CODE 39 + CHECKSUM'),
+    'C39'        => array('0123456789', 'CODE 39 - ANSI MH10.8M-1983 - USD-3 - 3 of 9'),
+    'C93'        => array('0123456789', 'CODE 93 - USS-93'),
+    'CODABAR'    => array('0123456789', 'CODABAR'),
+    'CODE11'     => array('0123456789', 'CODE 11'),
+    'EAN13'      => array('0123456789', 'EAN 13'),
+    'EAN2'       => array('12',         'EAN 2-Digits UPC-Based Extension'),
+    'EAN5'       => array('12345',      'EAN 5-Digits UPC-Based Extension'),
+    'EAN8'       => array('1234567',    'EAN 8'),
+    'I25+'       => array('0123456789', 'Interleaved 2 of 5 + CHECKSUM'),
+    'I25'        => array('0123456789', 'Interleaved 2 of 5'),
+    'IMB'        => array('0123456789', 'IMB - Intelligent Mail Barcode - Onecode - USPS-B-3200'),
+    'IMBPRE'     => array('fatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdf', 'IMB pre-processed'),
+    'KIX'        => array('0123456789', 'KIX (Klant index - Customer index)'),
+    'MSI+'       => array('0123456789', 'MSI + CHECKSUM (modulo 11)'),
+    'MSI'        => array('0123456789', 'MSI (Variation of Plessey code)'),
+    'PHARMA2T'   => array('0123456789', 'PHARMACODE TWO-TRACKS'),
+    'PHARMA'     => array('0123456789', 'PHARMACODE'),
+    'PLANET'     => array('0123456789', 'PLANET'),
+    'POSTNET'    => array('0123456789', 'POSTNET'),
+    'RMS4CC'     => array('0123456789', 'RMS4CC (Royal Mail 4-state Customer Bar Code)'),
+    'S25+'       => array('0123456789', 'Standard 2 of 5 + CHECKSUM'),
+    'S25'        => array('0123456789', 'Standard 2 of 5'),
+    'UPCA'       => array('0123456789', 'UPC-A'),
+    'UPCE'       => array('0123456789', 'UPC-E'),
+);
+$square = array(
+    'DATAMATRIX' => array('0123456789', 'DATAMATRIX (ISO/IEC 16022)'),
+    'PDF417'     => array('0123456789', 'PDF417 (ISO/IEC 15438:2006)'),
+    'QRCODE'     => array('0123456789', 'QR-CODE'),
+    'LRAW'       => array('0101010101', '1D RAW MODE (comma-separated rows of 01 strings)'),
+    'SRAW'       => array('0101,1010',  '2D RAW MODE (comma-separated rows of 01 strings)'),
+);
+
 $barcode = new \Com\Tecnick\Barcode\Barcode();
+
+$examples = '<h3>Linear</h3>'."\n";
+foreach ($linear as $type => $code) {
+    $bobj = $barcode->getBarcodeObj($type, $code[0], -3, -30);
+    $examples .= '<h4>[<span>'.$type.'</span>] '.$code[1].'</h4><p style="font-family:monospace;">'.$bobj->getHtmlDiv().'</p>'."\n";
+}
+
+$examples .= '<h3>Square</h3>'."\n";
+foreach ($square as $type => $code) {
+    $bobj = $barcode->getBarcodeObj($type, $code[0], -4, -4);
+    $examples .= '<h4>[<span>'.$type.'</span>] '.$code[1].'</h4><p style="font-family:monospace;">'.$bobj->getHtmlDiv().'</p>'."\n";
+}
+
 $bobj = $barcode->getBarcodeObj('QRCODE,H', 'http://www.tecnick.com', -4, -4, 'black');
 
 echo "
@@ -33,22 +88,27 @@ echo "
             table {border: 1px solid black;}
             th {border: 1px solid black;padding:4px;background-barcode:cornsilk;}
             td {border: 1px solid black;padding:4px;}
+            h3 {color:darkblue;}
+            h4 {color:darkgreen;}
+            h4 span  {color:firebrick;}
         </style>
     </head>
     <body>
-        <h1>Barcode Example</h1>
+        <h1>Usage example of tc-lib-barcode library</h1>
         <p>This is an usage example of <a href=\"https://github.com/tecnickcom/tc-lib-barcode\" title=\"tc-lib-barcode: PHP library to generate linear and bidimensional barcodes\">tc-lib-barcode</a> library.</p>
         <h2>Output Formats</h2>
-        <h3>Binary String</h3>
-        <pre style=\"font-family:monospace;\">".$bobj->getGrid()."</pre>
-        <h3>Unicode String</h3>
-        <pre style=\"font-family:monospace;line-height:100%;font-size:6px;\">".$bobj->getGrid(json_decode('"\u00A0"'), json_decode('"\u2588"'))."</pre>
-        <h3>HTML DIV</h3>
-        <p style=\"font-family:monospace;\">".$bobj->getHtmlDiv()."</p>
-        <h3>SVG Image</h3>
-        <p style=\"font-family:monospace;\">".$bobj->getSvgCode()."</p>
         <h3>PNG Image</h3>
         <p><img alt=\"Embedded Image\" src=\"data:image/png;base64,".base64_encode($bobj->getPngData())."\" /></p>
+        <h3>SVG Image</h3>
+        <p style=\"font-family:monospace;\">".$bobj->getSvgCode()."</p>
+        <h3>HTML DIV</h3>
+        <p style=\"font-family:monospace;\">".$bobj->getHtmlDiv()."</p>
+        <h3>Unicode String</h3>
+        <pre style=\"font-family:monospace;line-height:0.61em;font-size:6px;\">".$bobj->getGrid(json_decode('"\u00A0"'), json_decode('"\u2b1b"'))."</pre>
+        <h3>Binary String</h3>
+        <pre style=\"font-family:monospace;\">".$bobj->getGrid()."</pre>
+        <h2>Barcode Types</h2>
+        ".$examples."
     </body>
 </html>
 ";
