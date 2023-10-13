7b7d3475f82af39cc35d4232c9966ab7d70ad108
nicolaasuni <info@tecnick.com>
2023-10-13 19:10:48 +0100

    Aztec code draft 2

--- src/Type/Square/Aztec.php
+++ src/Type/Square/Aztec.php
@@ -16,8 +16,8 @@
 
 namespace Com\Tecnick\Barcode\Type\Square;
 
-use Com\Tecnick\Barcode\Exception as BarcodeException;
 use Com\Tecnick\Barcode\Type\Square\Aztec\Data;
+use Com\Tecnick\Barcode\Exception as BarcodeException;
 
 /**
  * Com\Tecnick\Barcode\Type\Square\Aztec
@@ -64,10 +64,48 @@ class Aztec extends \Com\Tecnick\Barcode\Type\Square
     protected $layers = 0;
 
     /**
+     * Extended Channel Interpretation (ECI) code.
+     * Valid codes are:
+     *      0: FNC1, Function 1 character
+     *      2: Cp437, Code page 437
+     *      3: ISO-8859-1, ISO/IEC 8859-1 - Latin-1 (Default encoding)
+     *      4: ISO-8859-2, ISO/IEC 8859-2 - Latin-2
+     *      5: ISO-8859-3, ISO/IEC 8859-3 - Latin-3
+     *      6: ISO-8859-4, ISO/IEC 8859-4 - Latin-4
+     *      7: ISO-8859-5, ISO/IEC 8859-5 - Latin/Cyrillic
+     *      8: ISO-8859-6, ISO/IEC 8859-6 - Latin/Arabic
+     *      9: ISO-8859-7, ISO/IEC 8859-7 - Latin/Greek
+     *     10: ISO-8859-8, ISO/IEC 8859-8 - Latin/Hebrew
+     *     11: ISO-8859-9, ISO/IEC 8859-9 - Latin-5
+     *     12: ISO-8859-10, ISO/IEC 8859-10 - Latin-6
+     *     13: ISO-8859-11, ISO/IEC 8859-11 - Latin/Thai
+     *     15: ISO-8859-13, ISO/IEC 8859-13 - Latin-7
+     *     16: ISO-8859-14, ISO/IEC 8859-14 - Latin-8 (Celtic)
+     *     17: ISO-8859-15, ISO/IEC 8859-15 - Latin-9
+     *     18: ISO-8859-16, ISO/IEC 8859-16 - Latin-10
+     *     20: Shift JIS
+     *     21: Cp1250, Windows-1250 - Superset of Latin-2
+     *     22: Cp1251, Windows-1251 - Latin/Cyrillic
+     *     23: Cp1252, Windows-1252 - Superset of Latin-1
+     *     24: Cp1256, Windows-1256 - Arabic
+     *     25: UTF-16BE, UnicodeBig, UnicodeBigUnmarked
+     *     26: UTF-8
+     *     27: US-ASCII
+     *     28: Big5
+     *     29: GB18030, GB2312, EUC_CN, GBK
+     *     30: EUC-KR
+     *
+     * @var int
+     */
+    protected $eci = -1;
+
+    /**
      * Set extra (optional) parameters:
      *     1: ECC - Error correction code percentage of error check words.
      *              A minimum of 23% + 3 words is recommended by ISO/IEC 24778:2008a.
      *     2: HINT - Encoding mode: B=Binary, D=Dynamic.
+     *     3: LAYERS - Custom number of layers (0 = auto).
+     *     4: ECI - Extended Channel Interpretation (ECI) code. Use -1 for FNC1. See $this->eci.
      */
     protected function setParameters()
     {
@@ -92,7 +130,16 @@ class Aztec extends \Com\Tecnick\Barcode\Type\Square
         ) {
             $this->params[0] = 0;
         }
-        $this->ecc = intval($this->params[0]);
+        $this->$layers = intval($this->params[2]);
+
+        // eci code. Used to set the charset encoding. See $this->eci.
+        if (
+            !isset($this->params[3]) ||
+            !in_array($this->params[3], array(2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,20,21,22,23,24,25,26,27,28,29,30))
+        ) {
+            $this->params[3] = -1;
+        }
+        $this->eci = intval($this->params[3]);
     }
 
     /**
--- src/Type/Square/Aztec/Encode.php
+++ src/Type/Square/Aztec/Encode.php
@@ -16,6 +16,7 @@
 
 namespace Com\Tecnick\Barcode\Type\Square\Aztec;
 
+use Com\Tecnick\Barcode\Type\Square\Aztec\Data;
 use Com\Tecnick\Barcode\Exception as BarcodeException;
 
 /**
@@ -33,7 +34,153 @@ use Com\Tecnick\Barcode\Exception as BarcodeException;
  */
 abstract class Encode
 {
-    protected function getHighLevelEncoding()
+    /**
+     * Current character encoding mode.
+     *
+     * @var int
+     */
+    protected $encmode = Data::MODE_UPPER;
+
+    /**
+     * Array containing the high-level encoding codewords.
+     *
+     * @var array
+     */
+    protected $cdws = array();// -> MODE_adCdw
+
+    /**
+     * Count the total number of bits.
+     *
+     * @var int
+     */
+    protected $totbits = 0;
+
+    /**
+     * Returns the high-level encoding for the given code and ECI mode.
+     *
+     * @param string $code The code to encode.
+     * @param int $eci The ECI mode to use.
+     *
+     * @return array
+     */
+    protected function getHighLevelEncoding($code, $eci = 0)
     {
+        $this->addFLG($eci);
+        $chars = array_values(unpack('C*', $code));
+        $maxidx = (count($chars) - 1);
+        for ($idx = 0; $idx < $maxidx; $idx += 2) {
+            $chr = $chars[$idx];
+            $nextchr = $chars[($idx + 1)];
+            $pmode = $this->pairMode($chr, $nextchr);
+            if ($pmode > 0) {
+                // add PUNCT pair
+                // ...
+            } else {
+                // add chars
+                // ...
+            }
+        }
+        return $this-> $cdws;
+    }
+
+    /**
+     * Returns the PUNCT two-bytes code if the given two characters are a punctuation pair.
+     * Punct codes 2–5 encode two bytes each.
+     *
+     * @param int $chr The current curacter code.
+     * @param int $nextchr The next character code.
+     *
+     * @return int
+     */
+    protected function pairMode($chr, $nextchr)
+    {
+        $key = (($chr << 8) + $nextchr);
+        switch ($key) {
+            case ((13 << 8) + 10): // '\r\n' (CR LF)
+                return 2;
+            case ((46 << 8) + 32): // '. ' (. SP)
+                return 3;
+            case ((44 << 8) + 32): // ', ' (, SP)
+                return 4;
+            case ((58 << 8) + 32): // ': ' (: SP)
+                return 5;
+        }
+        return 0;
+    }
+
+
+    protected function charEnc($mode, $ord)
+    {
+        return isset(DATA::CHAR_ENC[$mode][$ord]) ? DATA::CHAR_ENC[$mode][$ord] : 0;
+    }
+
+    protected function addRawCwd($bits, $value)
+    {
+        $this->$cdws[] = array($bits, $value);
+        $this->totbits += $bits;
+    }
+
+    protected function addCdw($mode, $value)
+    {
+        $this->addRawCwd(Data::MODE_BITS[$mode], $value);
+    }
+
+    protected function addLatch($mode)
+    {
+        if ($mode == $this->$encmode) {
+            return;
+        }
+        $latch = Data::LATCH_MAP[$this->$encmode][$mode];
+        foreach ($latch as $cdw) {
+            $this->$cdws[] = $cdw;
+            $this->totbits += $cdw[0];
+        }
+        $this->encmode = $mode;
+    }
+
+    protected function addShift($mode)
+    {
+        if ($this->$encmode == $mode) {
+            return $this->$encmode;
+        }
+        $shift = Data::SHIFT_MAP[$this->$encmode][$mode];
+        if (empty($shift)) {
+            return $this->$encmode;
+        }
+        foreach ($shift as $cdw) {
+            $this->$cdws[] = $cdw;
+            $this->totbits += $cdw[0];
+        }
+    }
+
+    protected function addFLG($eci)
+    {
+        if ($eci < 0) {
+            return;
+        }
+        $this->addShift(Data::MODE_PUNCT);
+        if ($eci == 0) {
+            $this->addRawCwd(3, 0); // FNC1
+            return;
+        }
+        $seci = (string)$eci;
+        $digits = strlen($seci);
+        $this->addRawCwd(3, $digits); // 1–6 digits
+        for ($idx = 0; $idx < $digits; $idx++) {
+            $this->addCdw(
+                Data::MODE_DIGIT,
+                charEnc(Data::MODE_DIGIT, ord($seci[$idx]))
+            );
+        }
+    }
+
+    protected function isSupportedChar($ord)
+    {
+        for ($mode = 0; $mode < 5; $mode++) {
+            if (isset(DATA::CHAR_ENC[$mode][$ord])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
